fae2a1369e23b54e2cd18ee1ce56518de600056e
Chris Stead <chrisstead@Mac.attlocal.net>
2026-01-20 14:39:04 -0800

    Added reset confirmation

--- src/components/ResetButton/index.js
+++ src/components/ResetButton/index.js
@@ -2,8 +2,15 @@ import React from 'react';
 import './ResetButton.css';
 
 export function ResetButton({ resetToDoItems }) {
+    function confirmAndReset() {
+        const userConfirmed = window.confirm('Are you sure you want to reset the to-do list? This action cannot be undone.');
+        if (userConfirmed) {
+            resetToDoItems();
+        }
+    }
+
     return (
-        <button className="reset-button" onClick={resetToDoItems}>
+        <button className="reset-button" onClick={confirmAndReset}>
             Reset List
         </button>
     );
